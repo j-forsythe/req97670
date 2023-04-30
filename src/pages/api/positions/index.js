b6befa3b041caec306208ed4a7c781052b0e48a5
Jill Forsythe <jill@legible.com>
2023-04-30 15:17:34 -0700

    feat: create positions REST APIs

--- src/pages/api/positions/index.js
+++ src/pages/api/positions/index.js
@@ -15,6 +15,33 @@ export default function handler(req, res) {
         res.status(200).json(data)
       })
       break
+    case 'POST':
+      let newPosition = req.body
+
+      jsonfile.readFile(file, 'utf-8', (err, data) => {
+        if (err) {
+          throw err
+        }
+
+        newPosition.id = data.length + 1
+
+        // add new record
+        data.push(newPosition)
+
+        // write JSON to a file
+        jsonfile.writeFile(file, data, { spaces: 4 }, (err) => {
+          if (err) {
+            throw err
+          }
+          console.log('JSON data is saved.')
+        })
+      })
+      // respond with new position data
+      res.status(201).json({
+        success: true,
+        data: newPosition,
+      })
+      break
     default: // Method not allowed
       res.status(405).end()
       break
