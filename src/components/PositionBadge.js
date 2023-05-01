ba402074bda522bd98b2da05b01073c9665fc5f5
Jill Forsythe <jill@legible.com>
2023-05-01 11:57:16 -0700

    feat: unfill position

--- src/components/PositionBadge.js
+++ src/components/PositionBadge.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UnfillPosition from './UnfillPosition'
 
 const PositionBadge = ({ employee, position }) => {
-  const filled = !!employee
   const { title, id: position_id } = position
   const { firstName, lastName, id: employeeId } = employee || {}
+  const [selected, setSelected] = useState(false)
+  const [filled, setFilled] = useState(!!position.employeeId)
   return (
-    <dl className="rounded-md border-2 border-emerald-900 max-w-fit inline-block">
+    <dl
+      className="rounded-md border-2 border-emerald-900 max-w-fit inline-block position-badge"
+      onClick={() => setSelected(!selected)}
+    >
       <dt>
         {title} {position_id}
       </dt>
@@ -22,6 +27,15 @@ const PositionBadge = ({ employee, position }) => {
           <strong>vacant</strong>
         </dd>
       )}
+      {selected ? (
+        <UnfillPosition
+          position={position}
+          onUnfillPosition={() => {
+            console.log('triggered')
+            setFilled(false)
+          }}
+        />
+      ) : null}
     </dl>
   )
 }
